870002d97237103a9596e30ed1c742af84fe42d2
hjh1398 <fhypothesis@gmail.com>
2023-08-24 16:33:17 +0900

    20230824

--- kurly/js/main.js
+++ kurly/js/main.js
@@ -18,3 +18,15 @@ categoryBtn.addEventListener('mouseleave', hideDropdownMenu);
 
 dropdownMenu.addEventListener('mouseenter', keepDropdownMenu);
 dropdownMenu.addEventListener('mouseleave', hideDropdownMenu);
+
+
+// 메뉴바 고정
+const menuBox = document.querySelector(".main-menu-box-wrapper");
+
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 0) {
+    menuBox.classList.add("fixed");
+  } else {
+    menuBox.classList.remove("fixed");
+  }
+});
